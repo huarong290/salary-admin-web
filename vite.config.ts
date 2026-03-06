1e3cfee81126a2f1fc4b7bd5965bc51d336aae4c
AliceZhang <huarong290@gmail.com>
2026-03-06 17:54:35 +0800

    chore: 项目初始化-样式目录架构初始化

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,34 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        // 配置路径别名，'@' 指向 'src' 目录
-        alias: {
-            '@': resolve(__dirname, 'src')
-        }
+  plugins: [vue()],
+  resolve: {
+    // 配置路径别名，'@' 指向 'src' 目录
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
-    server: {
-        port: 5173,
-        host: true, // 允许局域网访问
-        open: true, // 启动时自动打开浏览器
-        // 反向代理配置，解决本地开发跨域问题
-        proxy: {
-            '/api': {
-                target: 'http://localhost:28080', // 你的 SpringBoot 后端地址
-                changeOrigin: true,
-                // 如果后端接口没有 /api 前缀，需要在这里重写去掉
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // api: 'modern-compiler', // 如果控制台出现 sass 弃用警告，可以加上这一行
+        // 让 Vite 在每个 .vue 文件的 <style lang="scss"> 前面自动加上这句 @use
+        additionalData: `@use "@/assets/styles/variables.scss" as *;`,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: true, // 允许局域网访问
+    open: true, // 启动时自动打开浏览器
+    // 反向代理配置，解决本地开发跨域问题
+    proxy: {
+      '/api': {
+        target: 'http://localhost:28080', // 你的 SpringBoot 后端地址
+        changeOrigin: true,
+        // 如果后端接口没有 /api 前缀，需要在这里重写去掉
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  },
 });
